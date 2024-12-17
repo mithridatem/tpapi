05ff82a5d2e9d4aa4d223da5c310e07ea66ad247
mithridatem <mathieumithridate@adrar-formation.com>
2024-12-17 10:56:05 +0100

    feat: tools JsonResponse

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
-//chargement de l'autoloader de composer
-require_once './vendor/autoload.php';
 //chargement des variables d'environnement
 require_once './env.local.php';
+
+//chargement de l'autoloader de composer
+require_once './vendor/autoload.php';
+
 
 //utilisation de session_start(pour gérer la connexion au serveur)
 session_start();
@@ -12,15 +14,17 @@ $url = parse_url($_SERVER['REQUEST_URI']);
 //test si l'url posséde une route sinon on renvoi à la racine
 $path = $url['path'] ??  '/';
 
-//importer le UserController
+//importer les classes
 use App\Controller\UserController;
 use App\Utils\Tools;
+
+//instance du controller
 $userController = new UserController();
 
 //routeur
 switch ($path) {
     case '/tpapi/':
-        Tools::JsonResponse(["Message"=>"Bienvenue sur notre API","Valeur"=>"Mathieu"], 403);
+        Tools::JsonResponse(["Message"=>"Bienvenue sur notre API"], 200);
         break;
     case '/tpapi/user':
         $userController->save();
